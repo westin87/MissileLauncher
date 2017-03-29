01230266d02ab88a663e9ba2b0e3a5def65b5561
Westin <westin87@gmail.com>
2017-03-29 12:53:02 +0200

    Added more endpoints

--- missile_launcher/missile_launcher.py
+++ missile_launcher/missile_launcher.py
@@ -10,6 +10,7 @@ class Action(Enum):
     RIGHT = 0x08
     FIRE = 0x10
     STOP = 0x20
+    RESET = 0x50
 
 class MissileLauncher:
     def __init__(self):
@@ -33,29 +34,46 @@ class MissileLauncher:
     def stop(self):
         self._do(Action.STOP)
 
+    def reset(self):
+        self._do(Action.DOWN, 2000)
+        self._do(Action.LEFT, 8000)
+
     def execute(self, action, argument):
         if isinstance(action, str):
-            action = Action[action]
+            action = Action[action.capitalize()]
 
         if action in (Action.UP, Action.DOWN, Action.LEFT, Action.RIGHT):
-            milliseconds = argument
+            milliseconds = argument or 100
             self._move(action, milliseconds)
         elif action is Action.FIRE:
-            number_of_shots = argument
+            number_of_shots = argument or 1
             self._fire(number_of_shots)
+        elif action is Action.RESET:
+            self.reset()
         else:
             self._do(action)
 
     def _move(self, action, milliseconds):
+        milliseconds = _clip(milliseconds, 0, 10000)
         self._do(action)
         time.sleep(milliseconds / 1000)
         self._do(Action.STOP)
 
     def _fire(self, number_of_shots):
+        number_of_shots = _clip(number_of_shots, 1, 4)
         time.sleep(0.5)
         for _ in range(number_of_shots):
             self._do(Action.FIRE)
-            time.sleep(4.5)
+            time.sleep(4)
 
     def _do(self, action):
         self.usb_device.send_command(action.value)
+
+
+def _clip(value, min, max):
+    if value < min:
+        value = min
+    elif value > max:
+        value = max
+
+    return value
--- missile_launcher/rest_api.py
+++ missile_launcher/rest_api.py
@@ -1,17 +1,102 @@
-from flask import Flask
+import json
+
+from flask import Flask, request
 
 from missile_launcher.missile_launcher import MissileLauncher
 
 app = Flask(__name__)
+missile_launcher = MissileLauncher()
+
 
 @app.route("/fire", methods=['POST'])
 def fire():
-    missile_launcher = MissileLauncher()
-    missile_launcher.fire()
+    data = parse_request_data()
+    number_of_shots = data.get('number_of_shots', 1)
+    if isinstance(number_of_shots, int):
+        missile_launcher.fire(number_of_shots)
+    else:
+        return "Incorrect argument"
+
     return "Shooting"
+
+@app.route("/up", methods=['POST'])
+def up():
+    data = parse_request_data()
+    milliseconds = data.get('milliseconds', None)
+    if isinstance(milliseconds, int):
+        missile_launcher.up(milliseconds)
+    else:
+        return "Incorrect argument"
+
+    return "Turning up"
+
+
+@app.route("/down", methods=['POST'])
+def down():
+    data = parse_request_data()
+    milliseconds = data.get('milliseconds', None)
+    if isinstance(milliseconds, int):
+        missile_launcher.down(milliseconds)
+    else:
+        return "Incorrect argument"
+
+    return "Turning down"
+
+
+@app.route("/left", methods=['POST'])
+def left():
+    data = parse_request_data()
+    milliseconds = data.get('milliseconds', None)
+    if isinstance(milliseconds, int):
+        missile_launcher.left(milliseconds)
+    else:
+        return "Incorrect argument"
+
+    return "Turning left"
+
+
+@app.route("/right", methods=['POST'])
+def right():
+    data = parse_request_data()
+    milliseconds = data.get('milliseconds', None)
+    if isinstance(milliseconds, int):
+        missile_launcher.right(milliseconds)
+    else:
+        return "Incorrect argument"
+
+    return "Turning right"
+
+
+@app.route("/reset", methods=['POST'])
+def reset():
+    missile_launcher.reset()
+
+    return "Resetting"
+
+
+@app.route("/execute", methods=['POST'])
+def execute():
+    data = parse_request_data()
+    action = data.get('action', "")
+    argument = data.get('argument', None)
+    if action:
+        missile_launcher.execute(action, argument)
+
+    return "Turning right"
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "Pong"
+
+
+def parse_request_data():
+    data = json.loads(request.data)
+    return data
+
 
 def main():
     app.run(host='0.0.0.0', port=5000)
+
 
 if __name__ == "__main__":
     main()
