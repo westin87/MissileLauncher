f95208a5774835096479c77b8330918039a59707
System Administrator <root@Henrics-MacBook-Air.local>
2017-03-31 15:40:50 +0200

    Final adjustments

--- missile_launcher/build_watcher.py
+++ missile_launcher/build_watcher.py
@@ -1,6 +1,7 @@
 import signal
 from time import sleep
 
+from missile_launcher.coordinator import give_me_coords
 from missile_launcher.mail_reader import MailReader, get_name_from_mail
 from missile_launcher.missile_launcher import MissileLauncher
 
@@ -12,15 +13,20 @@ class BuildWatcher:
         self._should_run = False
 
     def start(self):
-        print()
+        print("Checking the mail!")
         self._should_run = True
 
         while self._should_run:
             new_mail = self._mail_reader.get_new_mail()
             if new_mail:
                 developer_name = get_name_from_mail(new_mail)
-                self._missile_launcher.reset()
-                self._missile_launcher.fire()
+                coords = give_me_coords(developer_name)
+
+                if coords:
+                    self._missile_launcher.reset()
+                    self._missile_launcher.up(coords[0])
+                    self._missile_launcher.right(coords[1])
+                    self._missile_launcher.fire()
 
             sleep(10)
 
---
+++ missile_launcher/coordinator.py
@@ -0,0 +1,14 @@
+import json
+
+from pathlib import Path
+
+def give_me_coords(self, name):
+    jsonpath = Path("/root/developers.json")
+    with jsonpath.open() as fo:
+        developer_list = json.load(fo)
+
+        for developer in developer_list:
+            if developer.get('Name', "") == name:
+                return developer.get('Up', 0), developer.get('Right', 0)
+
+    return None
