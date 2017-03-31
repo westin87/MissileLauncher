fbe93ef4cae3de396a89be3734917b0c93e99a02
Robin Westin <westin87@gmail.com>
2017-03-31 14:41:14 +0200

    added watcher

---
+++ missile_launcher/watcher.py
@@ -0,0 +1,34 @@
+import signal
+from time import sleep
+
+from missile_launcher.mail_reader import MailReader
+from missile_launcher.missile_launcher import MissileLauncher
+
+
+class MailWatcher:
+    def __init__(self):
+        self._mail_reader = MailReader()
+        self._missile_launcher = MissileLauncher()
+        self._should_run = False
+
+    def start(self):
+        self._should_run = True
+
+        while self._should_run:
+            new_mail = self._mail_reader.get_new_mail()
+            if new_mail:
+                self._missile_launcher.fire()
+
+            sleep(10)
+
+    def stop(self):
+        self._should_run = False
+
+
+def start_watcher():
+    watcher = MailWatcher()
+    signal.signal(signal.SIGINT, watcher.stop)
+
+
+if __name__ == '__main__':
+    start_watcher()
